c56ff2cea0b04b1b88d1d3eebc1e37dfdeae6216
hhar0022 <48737015+hhar0022@users.noreply.github.com>
2020-05-29 06:07:11 +1000

    Deleted since created another branch for the same

--- imageupload.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import json
-import base64
-s3_bucket = boto3.resource("s3").Bucket("weblens")
-def lambda_handler(event, context):
-    filename = event['filename']
-    file_content = base64.b64decode(event['body'])
-    s3_bucket.put_object(Key=filename,Body=file_content,ACL="public-read")
-    return {'statusCode': 200, 'body': json.dumps({'message': 'successful lambda function call'}), 'headers': {'Access-Control-Allow-Origin': '*'}}
