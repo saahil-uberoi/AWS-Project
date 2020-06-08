ea1a59eb85a2ac3c9bd9bd533763ea49aa7e31f2
Saahil Uberoi <shluberoi@gmail.com>
2020-06-08 18:54:11 +1000

    Update QueryHandler.py

--- QueryHandler.py
+++ QueryHandler.py
@@ -18,5 +18,11 @@ def lambda_handler(event, context):
     return {
             "isBase64Encoded": False,
             "statusCode": 200,
-            "headers": {},
-            "body": json.dumps({"links": links})}
+            "body": json.dumps({"links": links}),
+            "headers": {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET,POST'
+            }
+        
+    }
