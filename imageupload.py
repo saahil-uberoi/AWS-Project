fb154876a68392423e6453d014fea332a27a03a7
hhar0022 <48737015+hhar0022@users.noreply.github.com>
2020-06-08 16:12:09 +1000

    Updated imageupload.py to accomodate  UI changes

--- imageupload.py
+++ imageupload.py
@@ -3,7 +3,12 @@ import json
 import base64
 s3_bucket = boto3.resource("s3").Bucket("weblens")
 def lambda_handler(event, context):
-    filename = event['filename']
+    filename = "images/"+event["filename"]
     file_content = base64.b64decode(event['body'])
     s3_bucket.put_object(Key=filename,Body=file_content,ACL="public-read")
-    return {'statusCode': 200, 'body': json.dumps({'message': 'successful lambda function call'}), 'headers': {'Access-Control-Allow-Origin': '*'}}
+    return {'statusCode': 200, 'body': json.dumps({'message': 'successful lambda function call'}), 'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            }
+}
